9cc5549999e19cdb32414192fe0809d7469bea17
PHUC-A2 <phucban297@gmail.com>
2025-08-11 10:42:38 +0700

    #1 Hello World Shopzy

---
+++ src/test/java/com/example/shopzy/ShopzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

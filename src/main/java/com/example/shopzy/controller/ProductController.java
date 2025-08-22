29ab234d4c5e0d80ba5d026594e6cec9fa25abef
PHUC-A2 <phucban297@gmail.com>
2025-08-22 13:12:01 +0700

    #13 CRUD Product

---
+++ src/main/java/com/example/shopzy/controller/ProductController.java
@@ -0,0 +1,67 @@
+package com.example.shopzy.controller;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.shopzy.domain.Product;
+import com.example.shopzy.domain.response.ResultPaginationDTO;
+import com.example.shopzy.service.ProductService;
+import com.example.shopzy.util.annotation.ApiMessage;
+import com.example.shopzy.util.error.IdInvalidException;
+import com.turkraft.springfilter.boot.Filter;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/products")
+    @ApiMessage("Create a product")
+    public ResponseEntity<Product> createProduct(@RequestBody Product productReq) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.productService.createProduct(productReq));
+    }
+
+    @GetMapping("/products")
+    @ApiMessage("Get all product")
+    public ResponseEntity<ResultPaginationDTO> getAllProducts(
+            @Filter Specification spec, Pageable pageable) {
+        return ResponseEntity.ok(this.productService.getAllProducts(spec, pageable));
+    }
+
+    @GetMapping("/products/{id}")
+    @ApiMessage("Get product by id")
+    public ResponseEntity<Product> getProductById(@PathVariable("id") Long id) throws IdInvalidException {
+        return ResponseEntity.ok(this.productService.getProductByID(id));
+    }
+
+    @PutMapping("/products")
+    @ApiMessage("Update a product")
+    public ResponseEntity<Product> updateProduct(@RequestBody Product productReq) throws IdInvalidException {
+        Product product = this.productService.updateProduct(productReq);
+        return ResponseEntity.ok(this.productService.updateProduct(product));
+    }
+
+    @DeleteMapping("/products/{id}")
+    @ApiMessage("Delete a product")
+    public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long id) throws IdInvalidException {
+        this.productService.deleteProduct(id);
+        return ResponseEntity.ok(null);
+    }
+}
---
+++ src/main/java/com/example/shopzy/service/ProductService.java
@@ -0,0 +1,85 @@
+package com.example.shopzy.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import com.example.shopzy.domain.Product;
+import com.example.shopzy.domain.response.ResultPaginationDTO;
+import com.example.shopzy.repository.ProductRepository;
+import com.example.shopzy.util.error.IdInvalidException;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product createProduct(Product product) {
+        return this.productRepository.save(product);
+    }
+
+    public ResultPaginationDTO getAllProducts(Specification spec, Pageable pageable) {
+
+        Page<Product> pageProduct = this.productRepository.findAll(spec, pageable);
+
+        ResultPaginationDTO rs = new ResultPaginationDTO();
+        ResultPaginationDTO.Meta mt = new ResultPaginationDTO.Meta();
+
+        mt.setPage(pageable.getPageNumber());
+        mt.setPageSize(pageable.getPageSize());
+
+        mt.setPages(pageProduct.getTotalPages());
+        mt.setTotal(pageProduct.getTotalElements());
+
+        rs.setMeta(mt);
+        rs.setResult(pageProduct.getContent());
+
+        return rs;
+    }
+
+    public Product getProductByID(Long id) throws IdInvalidException {
+        Optional<Product> productOptional = this.productRepository.findById(id);
+        if (productOptional.isPresent()) {
+            return productOptional.get();
+        } else {
+            throw new IdInvalidException("Không tìm thấy Product với ID = " + id);
+        }
+    }
+
+    public Product updateProduct(Product productReq) throws IdInvalidException {
+        Product product = this.getProductByID(productReq.getId());
+
+        if (product == null) {
+            throw new IdInvalidException("Không tìm thấy Product với ID = " + productReq.getId());
+        }
+
+        product.setName(productReq.getName());
+        product.setDescription(productReq.getDescription());
+        product.setPrice(productReq.getPrice());
+        product.setStock(productReq.getStock());
+        // không cần update status vì tự động cập nhật dựa vào stock
+        product.setProductCondition(productReq.getProductCondition());
+        product.setImageUrl(productReq.getImageUrl());
+        product.setSize(productReq.getSize());
+        product.setColor(productReq.getColor());
+
+        return this.productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) throws IdInvalidException {
+
+        Product product = this.getProductByID(id);
+        if (product == null) {
+            throw new IdInvalidException("Không tìm thấy Product với ID = " + id);
+        }
+        this.productRepository.deleteById(id);
+    }
+
+}

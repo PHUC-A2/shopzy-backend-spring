f287713f11e90fe51583934d54885e98b5960fc8
PHUC-A2 <phucban297@gmail.com>
2025-08-31 01:35:04 +0700

    #20 Update endpoint CartItem, using (cart-items)

--- src/main/java/com/example/shopzy/controller/CartItemController.java
+++ src/main/java/com/example/shopzy/controller/CartItemController.java
@@ -1,6 +1,5 @@
 package com.example.shopzy.controller;
 
-
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
@@ -30,34 +29,34 @@ public class CartItemController {
         this.cartItemService = cartItemService;
     }
 
-    @PostMapping("/cartitems")
+    @PostMapping("/cart-items")
     @ApiMessage("Create cart item")
     public ResponseEntity<CartItem> createCartItem(@RequestBody CartItem cartItem) {
         CartItem created = this.cartItemService.createCartItem(cartItem);
         return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 
-    @GetMapping("/cartitems")
+    @GetMapping("/cart-items")
     @ApiMessage("get all cart items")
     public ResponseEntity<ResultPaginationDTO> getAllCartItems(@Filter Specification<CartItem> spec,
             Pageable pageable) {
         return ResponseEntity.ok(this.cartItemService.getAllCartItems(spec, pageable));
     }
 
-    @GetMapping("/cartitems/{id}")
+    @GetMapping("/cart-items/{id}")
     @ApiMessage("Get cart item by id")
     public ResponseEntity<CartItem> getCartItemById(@PathVariable("id") Long id) throws IdInvalidException {
         return ResponseEntity.ok(this.cartItemService.getCartItemById(id));
     }
 
-    @PutMapping("/cartitems")
+    @PutMapping("/cart-items/{id}")
     @ApiMessage("Update cart item")
     public ResponseEntity<CartItem> updateCartItem(@RequestBody CartItem cartItem) throws IdInvalidException {
         CartItem update = this.cartItemService.updateCartItem(cartItem);
         return ResponseEntity.ok(update);
     }
 
-    @DeleteMapping("/cartitems/{id}")
+    @DeleteMapping("/cart-items/{id}")
     @ApiMessage("Delete cart item")
     public ResponseEntity<Void> deleteCartItem(@PathVariable("id") Long id) throws IdInvalidException {
         this.cartItemService.deleteCartItem(id);
